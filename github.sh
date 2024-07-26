22cf0a23402eb172b00c4afe39fe500126eba88d
Oumatou <oumatoulacen@gmail.com>
2024-07-26 10:56:52 +0100

    chore: Update github.sh script to handle cron running from root directory

--- github.sh
+++ github.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # a script to run by cron for every 2 minute (run 'crontab -e' to change anything you want)
 
-# because cron runs from root / we need first to go to ourfile directory where our script exist
+# because cron runs from root(/) we need first to go to ourfile directory where our script exist
 cd /var/www/fsfe/
 git pull origin master --ff-only
